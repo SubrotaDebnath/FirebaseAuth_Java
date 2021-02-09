e32073747f7c6342b8563db55f5f7fa817ae8731
SubrotaDebnath <subrota.shuvro@gmail.com>
2021-02-10 02:19:50 +0600

    Initial commit

---
+++ app/src/main/java/subrota/shuvro/firebaseauth_java/Login.java
@@ -0,0 +1,14 @@
+package subrota.shuvro.firebaseauth_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
